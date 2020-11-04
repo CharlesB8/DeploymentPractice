cdb83290c18e4bcfcf333c34b52f2ce94538d2e3
Charles Bartels <charlieb2898@gmail.com>
2020-11-04 09:15:57 -0600

    first commit

---
+++ src/main/java/com/wozu/deployment/DogController.java
@@ -0,0 +1,18 @@
+package com.wozu.deployment;
+
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/breeds/")
+public class DogController {
+
+    @GetMapping("/image/random")
+    public String getDog(){
+
+        return "{\"message\":\"https://images.dog.ceo/breeds/pointer-germanlonghair/hans2.jpg\",\"status\":\"success\"}";
+
+    }
+}
